7c989b7eb35c97168322538d8ae0b6847c2a4f09
MeyerZhao <zhao_min_yuan@126.com>
2024-07-29 15:20:46 +0800

    step-05-theme: add toggle theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,njk}"],
+  // darkMode: 'selector',
+  darkMode: ['selector', '[data-mode="dark"]'],
   theme: {
     extend: {},
   },
