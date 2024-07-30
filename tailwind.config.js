279a7aa4c5b3a4203b7a9cfe76baf5740634d850
MeyerZhao <zhao_min_yuan@126.com>
2024-07-30 10:10:30 +0800

    step-06-ui: start bootstrap redesign.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: 'tw-',
   content: ["./src/**/*.{html,js,njk}"],
   // darkMode: 'selector',
   darkMode: ['selector', '[data-mode="dark"]'],
